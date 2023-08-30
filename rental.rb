0dde9a1da080821861e456c2c4604e4c1fdcc6eb
Anita Sharma <sharma.anita00001@gmail.com>
2023-08-30 15:49:44 +0545

    Create Rental class

--- rental.rb
+++ rental.rb
@@ -1,7 +1,10 @@
 class Rental
   attr_accessor :date
+  attr_reader :book, :person
 
-  def initialize(date)
+  def initialize(person, book, date)
     @date = date
+    @person = person
+    @book = book
   end
 end
