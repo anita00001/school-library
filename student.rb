180e80a4b031eecc2fa0df04df2968dc5bfe9842
Anita Sharma <sharma.anita00001@gmail.com>
2023-08-30 12:31:09 +0545

    Create has-many relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -4,6 +4,7 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
+    classroom.add_student(self) # Add the current student to the classroom
   end
 
   def play_hooky
